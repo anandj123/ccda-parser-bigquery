1dbc87b9e754e0d9a73efe1249d726d41ef97161
Anand Jain <jain.12@gmail.com>
2022-12-10 20:50:00 -0500

    error

--- ccda-parser-to-bigquery.py
+++ ccda-parser-to-bigquery.py
@@ -123,7 +123,7 @@ def parse():
                 p.stdin.close()
 
                 if p.returncode != 0:
-                    print(error)
+                    print(error.replace("\n",'\n'))
                     print("Failed to parse clinical XML at %s" % blob.name)
                     continue                
 
