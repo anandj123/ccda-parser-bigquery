b7d8749b6651d3aec9d8ffff0a7f131642847979
Anand Jain <jain.12@gmail.com>
2022-12-14 11:16:48 -0500

    lauch script

--- build/launch-ccda-batch-custom.sh
+++ build/launch-ccda-batch-custom.sh
@@ -4,5 +4,7 @@
 #--location us-east-1 \
 #--config ccda-batch-config-custom.json
 
-curl -d "@ccda-batch-config-custom.json" -X \
-POST https://batch.googleapis.com/v1/projects/anand-bq-test-2/locations/us-east-1/jobs?job_id=ccda-parser-job-$(date +%s)
+curl -d "@ccda-batch-config-custom.json" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+-X POST https://batch.googleapis.com/v1/projects/anand-bq-test-2/locations/us-east1/jobs?job_id=ccda-parser-job-$(date +%s)
